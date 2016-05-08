8290c3ccf142e7a0d45fcab018861ec66681699c
Sooraj <soorajraju@gmail.com>
2016-05-08 02:28:37 -0400

    Start date and Risk calculation
    Signed-off-by: Sooraj <soorajraju@gmail.com>

--- 01_DownloadData.py
+++ 01_DownloadData.py
@@ -3,12 +3,26 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 from googlefinance import getQuotes
 import json
+import numpy as np
 
-BSE = DataReader("^BSESN",  'yahoo', datetime(2016,3,1), datetime(2016,4,20))
-SBI = DataReader("SBIN.BO", 'yahoo', datetime(2016,3,1), datetime(2016,4,20))
+StartDate = datetime(2016, 1, 1, 0, 00)
+EndDate = datetime(2016, 5, 1, 0, 00)
+BSE = DataReader("^BSESN",  'yahoo', StartDate, EndDate)
+SBI = DataReader("SBIN.BO", 'yahoo', StartDate, EndDate)
+LT = DataReader("LT.BO", 'yahoo', StartDate, EndDate)
+
+print BSE.describe()
+
 #print ibm
 plt.figure(1)
-plt.subplot(211)
+plt.subplot(311)
 plt.plot(BSE['Adj Close'],'r--')
-plt.subplot(212)         
+plt.subplot(312)         
 plt.plot(SBI['Adj Close'],'g--')
+plt.subplot(313)         
+plt.plot(LT['Adj Close'],'b--')
+
+RiskSbi = np.mean(SBI['Adj Close'])/np.std(SBI['Adj Close'])
+print RiskSbi
+RiskLt = np.mean(LT['Adj Close'])/np.std(LT['Adj Close'])
+print RiskLt
